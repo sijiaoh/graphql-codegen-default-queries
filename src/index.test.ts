b00aafe176dd91d25ea57b14339cc1146e5948e9
Sijia Wang <sijiaoh@outlook.com>
2021-10-20 13:15:37 +0900

    Add simple query implement.

---
+++ src/index.test.ts
@@ -0,0 +1,47 @@
+import {buildSchema, parse, introspectionFromSchema} from 'graphql';
+import {plugin} from '../src/index';
+
+describe('graphql-codegen-default-queries', () => {
+  describe(plugin.name, () => {
+    const checkSimilarString = (a: string, b: string) => {
+      const normalize = (str: string) => str.replace(/\s/g, '');
+      expect(normalize(a)).toBe(normalize(b));
+    };
+
+    const checkOutput = async (input: string, output: string) => {
+      const schema = buildSchema(input);
+      const content = await plugin(schema, [], {includeDirectives: false});
+      checkSimilarString(content.toString(), output);
+    };
+
+    describe('return scalar query', () => {
+      it('return correct output', async () => {
+        await checkOutput(
+          /* GraphQL */ `
+            type Query {
+              hello: String!
+            }
+          `,
+          /* GraphQL */ `
+            query hello {
+              hello
+            }
+          `
+        );
+
+        await checkOutput(
+          /* GraphQL */ `
+            type Query {
+              hello: String
+            }
+          `,
+          /* GraphQL */ `
+            query hello {
+              hello
+            }
+          `
+        );
+      });
+    });
+  });
+});
--- src/index.ts
+++ src/index.ts
@@ -3,12 +3,38 @@ import {
   PluginFunction,
   PluginValidateFn,
 } from '@graphql-codegen/plugin-helpers';
-import {buildScalarsFromConfig} from '@graphql-codegen/visitor-plugin-common';
+import {
+  introspectionFromSchema,
+  IntrospectionObjectType,
+  IntrospectionOutputTypeRef,
+} from 'graphql';
 
 export const plugin: PluginFunction = schema => {
-  const scalars = buildScalarsFromConfig(schema, {});
-  console.log(scalars);
-  return '';
+  const queryType = introspectionFromSchema(schema).__schema.types.find(
+    t => t.name === 'Query'
+  ) as IntrospectionObjectType | undefined;
+
+  if (!queryType) throw new Error('Query not found');
+
+  const file = queryType.fields
+    .map(field => {
+      let type: IntrospectionOutputTypeRef;
+      if (field.type.kind === 'NON_NULL') type = field.type.ofType;
+      else type = field.type;
+
+      if (type.kind === 'SCALAR') {
+        return `
+          query ${field.name} {
+            ${field.name}
+          }
+        `;
+      }
+
+      return '';
+    })
+    .join('\n');
+
+  return file;
 };
 
 export const validate: PluginValidateFn = (
