a17a8690cf9d4d3c5237371cc386cafd2809653d
Sijia Wang <sijiaoh@outlook.com>
2021-10-20 15:20:14 +0900

    Add object query inprement.

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,47 +1,77 @@
 import {buildSchema} from 'graphql';
 import {plugin} from '../src/index';
 
-describe('graphql-codegen-default-queries', () => {
-  describe(plugin.name, () => {
-    const checkSimilarString = (a: string, b: string) => {
-      const normalize = (str: string) => str.replace(/\s/g, '');
-      expect(normalize(a)).toBe(normalize(b));
-    };
+describe(plugin.name, () => {
+  const checkSimilarString = (a: string, b: string) => {
+    const normalize = (str: string) => str.replace(/\s/g, '');
+    expect(normalize(a)).toBe(normalize(b));
+  };
 
-    const checkOutput = async (input: string, output: string) => {
-      const schema = buildSchema(input);
-      const content = await plugin(schema, [], {includeDirectives: false});
-      checkSimilarString(content.toString(), output);
-    };
+  const checkOutput = async (input: string, output: string) => {
+    const schema = buildSchema(input);
+    const content = await plugin(schema, [], {includeDirectives: false});
+    checkSimilarString(content.toString(), output);
+  };
 
-    describe('return scalar query', () => {
-      it('return correct output', async () => {
-        await checkOutput(
-          /* GraphQL */ `
-            type Query {
-              hello: String!
-            }
-          `,
-          /* GraphQL */ `
-            query hello {
-              hello
-            }
-          `
-        );
+  describe('return scalar query', () => {
+    it('return correct output', async () => {
+      await checkOutput(
+        /* GraphQL */ `
+          type Query {
+            hello: String!
+          }
+        `,
+        /* GraphQL */ `
+          query hello {
+            hello
+          }
+        `
+      );
 
-        await checkOutput(
-          /* GraphQL */ `
-            type Query {
-              hello: String
+      await checkOutput(
+        /* GraphQL */ `
+          type Query {
+            hello: String
+          }
+        `,
+        /* GraphQL */ `
+          query hello {
+            hello
+          }
+        `
+      );
+    });
+  });
+
+  describe('return object query', () => {
+    it('return correct output', async () => {
+      await checkOutput(
+        /* GraphQL */ `
+          type User {
+            id: Int
+            name: String
+          }
+
+          type Query {
+            me1: User
+            me2: User!
+          }
+        `,
+        /* GraphQL */ `
+          query me1 {
+            me1 {
+              id
+              name
             }
-          `,
-          /* GraphQL */ `
-            query hello {
-              hello
+          }
+          query me2 {
+            me2 {
+              id
+              name
             }
-          `
-        );
-      });
+          }
+        `
+      );
     });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+import {EOL} from 'os';
 import {extname} from 'path';
 import {
   PluginFunction,
@@ -12,31 +13,47 @@ import {
 } from 'graphql';
 
 export const plugin: PluginFunction = schema => {
-  const queryType = introspectionFromSchema(schema).__schema.types.find(
-    t => t.name === 'Query'
-  ) as IntrospectionObjectType | undefined;
+  const types = introspectionFromSchema(schema).__schema.types;
+  const queryType = types.find(t => t.name === 'Query') as
+    | IntrospectionObjectType
+    | undefined;
 
-  if (!queryType) throw new Error('Query not found');
+  if (!queryType) throw new Error('Query not found!');
 
   const file = queryType.fields
     .map(field => {
-      let type:
+      let fieldType:
         | IntrospectionNamedTypeRef<IntrospectionOutputType>
         // eslint-disable-next-line @typescript-eslint/no-explicit-any
         | IntrospectionListTypeRef<any>;
 
-      if (field.type.kind === 'NON_NULL') type = field.type.ofType;
-      else type = field.type;
+      if (field.type.kind === 'NON_NULL') fieldType = field.type.ofType;
+      else fieldType = field.type;
 
-      if (type.kind === 'SCALAR') {
+      if (fieldType.kind === 'OBJECT') {
+        const name = fieldType.name;
+        const returnType = types.find(type => type.name === name) as
+          | IntrospectionObjectType
+          | undefined;
+
+        if (!returnType) throw new Error('Return type not found!');
+
+        const fields = returnType.fields.map(field => field.name).join(EOL);
+
+        return `
+          query ${field.name} {
+            ${field.name} {
+              ${fields}
+            }
+          }
+        `;
+      } else {
         return `
           query ${field.name} {
             ${field.name}
           }
         `;
       }
-
-      return '';
     })
     .join('\n');
 
